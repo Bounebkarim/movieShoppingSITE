f8c785e0732a4095b31ae38b72750200f06ef5d7
karim-bouneb <karimbouneb@gmail.com>
2022-06-10 19:06:23 +0100

    Add project files.

---
+++ movieShoppingSITE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace movieShoppingSITE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
